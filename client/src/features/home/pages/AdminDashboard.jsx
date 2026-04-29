c24e1b1221eaae20f28fabb8f3c3e87cffe8d611
Dhirendra Bam <dhirendrabam12345@gmail.com>
2026-04-29 12:12:29 +0545

    updated all responsive code

--- client/src/features/home/pages/AdminDashboard.jsx
+++ client/src/features/home/pages/AdminDashboard.jsx
@@ -2,6 +2,12 @@ const AdminDashboard = () => {
   return (
     <div>
       <h2>Admin</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugit
+        soluta eveniet. Tempore at accusantium debitis optio necessitatibus odit
+        illo sunt nesciunt ipsum excepturi. Obcaecati dolores doloremque id
+        adipisci libero!
+      </p>
     </div>
   );
 };
--- client/src/features/home/pages/EmployeeDashboard.jsx
+++ client/src/features/home/pages/EmployeeDashboard.jsx
@@ -2,6 +2,12 @@ const EmployeeDashboard = () => {
   return (
     <div>
       <h2>Employee</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+        officia accusantium, fuga fugit voluptate eum inventore necessitatibus
+        ea autem unde! Commodi excepturi non suscipit architecto quas fuga porro
+        enim in.
+      </p>
     </div>
   );
 };
