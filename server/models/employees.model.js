e3fe9a8ba6d1efc61feb34d99c00d24fc48d834d
dhirendrabam001 <dhirendrabam12345@gmail.com>
2026-05-05 09:31:04 +0530

    updated new code

--- server/models/employees.model.js
+++ server/models/employees.model.js
@@ -22,6 +22,28 @@ const employeesSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
+const employeSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    phone: { type: String, required: true },
+    date: { type: Date },
+    bio: { type: String, required: true, trim: true },
+    department: { type: String },
+    position: { type: String, required: true },
+    salary: { type: Number, required: true },
+    allowance: { type: Number, default: 0 },
+    deduction: { type: String, default: 0 },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "employee"],
+    },
+  },
+  { timestamps: true },
+);
+
 const Employee = mongoose.model("Employee", employeesSchema);
 
 module.exports = {
