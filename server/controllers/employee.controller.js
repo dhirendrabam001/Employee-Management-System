64ddc675ac4cdc571212ad4b29838902eb86153f
dhirendrabam001 <dhirendrabam12345@gmail.com>
2026-05-16 17:23:59 +0530

    updated employee profile

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -113,7 +113,7 @@ const getEmployeeById = async (req, res) => {
     const employee = await Employee.findById(employeeId);
     if (!employee) {
       return res
-        .status(400)
+        .status(404)
         .json({ success: false, message: "Employee does not found!" });
     }
 
@@ -130,4 +130,91 @@ const getEmployeeById = async (req, res) => {
   }
 };
 
-module.exports = { employee, getAllEmployeeList, getEmployeeById };
+// update profile
+const updateEmployeeProfile = async (req, res) => {
+  try {
+    const employeeId = req.params.id;
+    const {
+      firstName,
+      lastName,
+      phone,
+      date,
+      bio,
+      department,
+      position,
+      salary,
+      allowance,
+      deduction,
+      status,
+      email,
+      password,
+      role,
+    } = req.body;
+
+    const employee = await Employee.findById(employeeId);
+    if (!employee) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Employee does not found" });
+    }
+
+    // Update fields (only if provided)
+    if (firstName) employee.firstName = firstName;
+    if (lastName) employee.lastName = lastName;
+    if (phone) employee.phone = phone;
+    if (date) employee.date = date;
+    if (bio) employee.bio = bio;
+    if (department) employee.department = department;
+    if (position) employee.position = position;
+    if (salary) employee.salary = salary;
+    if (allowance) employee.allowance = allowance;
+    if (deduction) employee.deduction = deduction;
+    if (status) employee.status = status;
+    if (email) employee.email = email;
+    if (role) employee.role = role;
+
+    // change passowrd safely
+    if (password) {
+      const hashPassword = await bcrypt.hash(password, 10);
+      employee.password = hashPassword;
+    }
+
+    await employee.save();
+
+    return res.status(200).json({
+      success: true,
+      employee: {
+        id: employee._id,
+        firstName: employee.firstName,
+        lastName: employee.lastName,
+        phone: employee.phone,
+        date: employee.date,
+        bio: employee.bio,
+        department: employee.department,
+        position: employee.position,
+        salary: employee.salary,
+        allowance: employee.allowance,
+        deduction: employee.deduction,
+        status: employee.status,
+        email: employee.email,
+        password: employee,
+        password,
+        role: employee.role,
+      },
+      message: "Employee Profile Updated Successfully!",
+    });
+    // update employee data
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+module.exports = {
+  employee,
+  getAllEmployeeList,
+  getEmployeeById,
+  updateEmployeeProfile,
+};
--- server/routes/employee.routes.js
+++ server/routes/employee.routes.js
@@ -3,6 +3,7 @@ const {
   employee,
   getAllEmployeeList,
   getEmployeeById,
+  updateEmployeeProfile,
 } = require("../controllers/employee.controller");
 const auth = require("../middleware/auth");
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/addEmployee", auth, employee);
 router.get("/getEmployeeList", auth, getAllEmployeeList);
 router.get("/getEmployeeID/:id", auth, getEmployeeById);
+router.put("/updateEmployeeProfile/:id", auth, updateEmployeeProfile);
 
 module.exports = router;
