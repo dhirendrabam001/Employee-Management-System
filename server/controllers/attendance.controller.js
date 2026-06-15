502672f90eabde366202f94a37781c4ee428f482
Dhirendra Bam <dhirendrabam12345@gmail.com>
2026-06-15 22:55:51 +0545

    updated attendance controller

--- server/controllers/attendance.controller.js
+++ server/controllers/attendance.controller.js
@@ -1,6 +1,9 @@
 const { Attendance } = require("../models/employee.attendance.model");
+const { User } = require("../models/user.model");
+
 const attendance = async (req, res) => {
   try {
+    const employeeId = req.params.id;
     const {
       employee,
       attendanceDate,
@@ -12,6 +15,23 @@ const attendance = async (req, res) => {
       totalWorkingHrs,
     } = req.body;
     console.log(req.body);
+
+    // all field are required validation
+    if (
+      !employee ||
+      !attendanceDate ||
+      !clockInTime ||
+      !workLocation ||
+      !shiftTime
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please all field are requried" });
+    }
+
+    // check employee exit or not
+    const employeeExisting = await User.findById(employeeId);
+    console.log("employee", employeeExisting);
   } catch (error) {
     console.error(error);
     return res
