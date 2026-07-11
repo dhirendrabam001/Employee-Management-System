65e8562c1c80f692793643fabf553463cbf89043
Dhirendra Bam <dhirendrabam12345@gmail.com>
2026-07-11 15:42:06 +0545

    frontend deploye error fix

--- client/src/utils/constantUrl.js
+++ client/src/utils/constantUrl.js
@@ -1,11 +1,12 @@
 // const BASE_URL = "http://localhost:9000";
-const LIVE_URL = "https://employee-management-system-7qpx.onrender.com";
 
 // export const USER_API_END_POINT = `${BASE_URL}/api/user`;
 // export const EMPLOYEE_API_END_POINT = `${BASE_URL}/api/employee`;
 // export const PAYSLIPS_API_END_POINT = `${BASE_URL}/api/payslips`;
 // export const ATTENDANCE_API_END_POINT = `${BASE_URL}/api/attendance`;
 // export const LEAVE_API_END_POINT = `${BASE_URL}/api/leave`;
+
+const LIVE_URL = "https://employee-management-system-7qpx.onrender.com";
 
 export const USER_API_END_POINT = `${LIVE_URL}/api/user`;
 export const EMPLOYEE_API_END_POINT = `${LIVE_URL}/api/employee`;
