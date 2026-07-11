74ee651ef1d8a73f4d82cafb4b8e7c38e258a2c7
Dhirendra Bam <dhirendrabam12345@gmail.com>
2026-07-11 16:10:06 +0545

    apis changes

--- client/src/utils/constantUrl.js
+++ client/src/utils/constantUrl.js
@@ -1,18 +1,18 @@
-// const BASE_URL = "http://localhost:9000";
+const BASE_URL = "http://localhost:9000";
 
-// export const USER_API_END_POINT = `${BASE_URL}/api/user`;
-// export const EMPLOYEE_API_END_POINT = `${BASE_URL}/api/employee`;
-// export const PAYSLIPS_API_END_POINT = `${BASE_URL}/api/payslips`;
-// export const ATTENDANCE_API_END_POINT = `${BASE_URL}/api/attendance`;
-// export const LEAVE_API_END_POINT = `${BASE_URL}/api/leave`;
+export const USER_API_END_POINT = `${BASE_URL}/api/user`;
+export const EMPLOYEE_API_END_POINT = `${BASE_URL}/api/employee`;
+export const PAYSLIPS_API_END_POINT = `${BASE_URL}/api/payslips`;
+export const ATTENDANCE_API_END_POINT = `${BASE_URL}/api/attendance`;
+export const LEAVE_API_END_POINT = `${BASE_URL}/api/leave`;
 
-const LIVE_URL = "https://employee-management-system-7qpx.onrender.com";
+// const LIVE_URL = "https://employee-management-system-7qpx.onrender.com";
 
-export const USER_API_END_POINT = `${LIVE_URL}/api/user`;
-export const EMPLOYEE_API_END_POINT = `${LIVE_URL}/api/employee`;
-export const PAYSLIPS_API_END_POINT = `${LIVE_URL}/api/payslips`;
-export const ATTENDANCE_API_END_POINT = `${LIVE_URL}/api/attendance`;
-export const LEAVE_API_END_POINT = `${LIVE_URL}/api/leave`;
+// export const USER_API_END_POINT = `${LIVE_URL}/api/user`;
+// export const EMPLOYEE_API_END_POINT = `${LIVE_URL}/api/employee`;
+// export const PAYSLIPS_API_END_POINT = `${LIVE_URL}/api/payslips`;
+// export const ATTENDANCE_API_END_POINT = `${LIVE_URL}/api/attendance`;
+// export const LEAVE_API_END_POINT = `${LIVE_URL}/api/leave`;
 
 
 
